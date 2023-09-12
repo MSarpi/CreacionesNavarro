7cf4dc4ad3b5054b8dfa7e38fb0a7cdd9b2c2a8d
MSarpi <miguel.sarpi@gmail.com>
2023-09-12 13:31:45 -0300

    fix navbar and contact

--- js/js.js
+++ js/js.js
@@ -1,3 +1,28 @@
+document.getElementById("toastbtn").onclick = function() {
+  var toastElList = [].slice.call(document.querySelectorAll('.toast'))
+  var toastList = toastElList.map(function(toastEl) {
+  // Creates an array of toasts (it only initializes them)
+    return new bootstrap.Toast(toastEl) // No need for options; use the default options
+  });
+ toastList.forEach(toast => toast.show()); // This show them
+
+  console.log(toastList); // Testing to see if it works
+
+};
+
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos < 90) {
+      document.getElementById("navbar-smoth").style.top = "0";
+  } else {
+      document.getElementById("navbar-smoth").style.top = "-300px";
+  }
+  prevScrollpos = currentScrollPos;
+}
+
 const scrollToTopBtn = document.getElementById("scrollToTopBtn");
 
 // Mostrar el botón cuando el usuario haya bajado 20% de la altura de la página
@@ -17,25 +42,6 @@ window.scrollTo({
 });
 });
 
-const navbar = document.getElementById("navbar-smoth");
-
-// Mostrar el botón cuando el usuario haya bajado 20% de la altura de la página
-window.addEventListener("scroll", () => {
-if (window.scrollY > window.innerHeight * 0.9) {
-  navbar.style.opacity = "0";
-  
-} else {
-  navbar.style.opacity = "1";
-}
-});
-
-// Volver al principio de la página cuando se haga clic en el botón
-navbar.addEventListener("click", () => {
-window.scrollTo({
-  top: 0,
-  behavior: "smooth" // Suavizar el scroll para que sea más agradable
-});
-});
 
 function productos_conserjes() {
   $('#conserjes').modal('show');
