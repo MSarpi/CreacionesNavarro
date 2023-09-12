9e1d93963015e5b6047377fa4d24bfe026916dfe
MSarpi <81937827+MSarpi@users.noreply.github.com>
2023-09-11 23:55:55 -0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,38 @@
+const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+
+// Mostrar el botón cuando el usuario haya bajado 20% de la altura de la página
+window.addEventListener("scroll", () => {
+if (window.scrollY > window.innerHeight * 0.2) {
+  scrollToTopBtn.style.opacity = "1";
+} else {
+  scrollToTopBtn.style.opacity = "0";
+}
+});
+
+// Volver al principio de la página cuando se haga clic en el botón
+scrollToTopBtn.addEventListener("click", () => {
+window.scrollTo({
+  top: 0,
+  behavior: "smooth" // Suavizar el scroll para que sea más agradable
+});
+});
+
+const navbar = document.getElementById("navbar");
+
+// Mostrar el botón cuando el usuario haya bajado 20% de la altura de la página
+window.addEventListener("scroll", () => {
+if (window.scrollY > window.innerHeight * 0.9) {
+  navbar.style.opacity = "0";
+} else {
+  navbar.style.opacity = "1";
+}
+});
+
+// Volver al principio de la página cuando se haga clic en el botón
+navbar.addEventListener("click", () => {
+window.scrollTo({
+  top: 0,
+  behavior: "smooth" // Suavizar el scroll para que sea más agradable
+});
+});
+
